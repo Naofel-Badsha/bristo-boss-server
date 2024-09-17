35874d13e22fb19e78bd97bd27f8a3bf1837a9f4
programming hero <naofelbadsha@gmail.com>
2024-09-17 20:21:29 +0100

    added make user admin patch api

--- index.js
+++ index.js
@@ -84,6 +84,20 @@ async function run() {
     res.send(result)
   });
 
+  //------Make---User-------Admin-------patch-------
+  app.patch('/users/admin/:id', async(req, res) => {
+    const id = req.params.id;
+    const filter = {_id: new ObjectId(id)};
+    const updateDoc = {
+      $set: {
+        role: 'admin'
+      },
+    };
+    const result = await userCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  })
+
+
   //------------Delete-------A-------Users--------
   app.delete('/users/:id', async(req, res) => {
     const id = req.params.id;
