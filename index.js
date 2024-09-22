51d69eb0d5253a725b8adc0665606f94082d2d6b
programming hero <naofelbadsha@gmail.com>
2024-09-22 03:51:09 +0100

    added adminHome all card and chart data get by database

--- index.js
+++ index.js
@@ -314,7 +314,43 @@ async function run() {
   });
 
 
-  //-------Order---- status-----
+  //-------using-------aggregate-------pipeline-----
+  app.get('/order-stats', verifyToken, verifyAdmin, async(req, res) => {
+    const result = await paymentCollection.aggregate([
+    {
+      $unwind: '$menuItemIds'
+    },
+    {
+      $lookup: {
+        from: 'menu',
+        localField: 'menuItemIds',
+        foreignField: '_id',
+        as: 'menuItems'
+      }
+    },
+    {
+      $unwind: '$menuItems' 
+    },
+    {
+      $group: {
+        _id: '$menuItems.category',
+        quantity: {$sum: 1},
+        revenue: {$sum: '$menuItems.price'}
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        category: '$_id',
+        quantity: '$quantity',
+        revenue:'$revenue' 
+
+      }
+    }
+    
+    ]).toArray();
+    res.send(result);
+  })
 
 
 
